082fb615af9883c910e1be5736bc6ae29bc5b5a1
not-elm <elmgameinfo@gmail.com>
2024-12-13 15:09:44 +0900

    update: append base_dir to exists

--- crates/api/src/fs/exists.rs
+++ crates/api/src/fs/exists.rs
@@ -1,8 +1,10 @@
-use crate::fs::{error_if_not_accessible, FsScope};
+use crate::fs::{error_if_not_accessible, join_path_if_need, BaseDirectory, FsScope};
 use crate::macros::define_api_plugin;
 use bevy_ecs::system::{In, Res};
 use bevy_flurx::action::{once, Action};
 use bevy_flurx_ipc::command;
+use serde::Deserialize;
+use std::path::PathBuf;
 
 define_api_plugin!(
     /// You'll be able to check if the path exists from typescript(or js).
@@ -16,15 +18,22 @@ define_api_plugin!(
     command: exists
 );
 
+#[derive(Deserialize, Default)]
+struct Args {
+    path: PathBuf,
+    dir: Option<BaseDirectory>,
+}
+
 #[command(id = "FLURX|fs::exists", internal)]
-fn exists(In(args): In<String>) -> Action<String, Result<bool, String>> {
+fn exists(In(args): In<Args>) -> Action<Args, Result<bool, String>> {
     once::run(exists_system).with(args)
 }
 
 fn exists_system(
-    In(path): In<String>,
+    In(args): In<Args>,
     scope: Option<Res<FsScope>>,
 ) -> Result<bool, String> {
+    let path = join_path_if_need(&args.dir, args.path);
     error_if_not_accessible(&path, &scope)?;
     std::fs::exists(path).map_err(|e| e.to_string())
 }
@@ -33,9 +42,10 @@ fn exists_system(
 #[cfg(test)]
 //noinspection DuplicatedCode
 mod tests {
-    use crate::fs::exists::exists_system;
+    use crate::fs::exists::{exists_system, Args};
     use crate::fs::FsScope;
     use crate::tests::test_app;
+    use bevy::utils::default;
     use bevy_app::{Startup, Update};
     use bevy_ecs::prelude::Commands;
     use bevy_flurx::action::once;
@@ -47,7 +57,10 @@ mod tests {
         app.add_systems(Startup, |mut commands: Commands| {
             commands.spawn(Reactor::schedule(|task| async move {
                 let tmp_dir = std::env::temp_dir();
-                let result: Result<_, _> = task.will(Update, once::run(exists_system).with(tmp_dir.to_str().unwrap().to_string())).await;
+                let result: Result<_, _> = task.will(Update, once::run(exists_system).with(Args {
+                    path: tmp_dir,
+                    ..default()
+                })).await;
                 assert!(result.unwrap());
             }));
         });
@@ -61,7 +74,10 @@ mod tests {
             commands.spawn(Reactor::schedule(|task| async move {
                 let tmp_dir = std::env::temp_dir();
                 let not_exists_dir = tmp_dir.join("not_exists");
-                let result: Result<_, _> = task.will(Update, once::run(exists_system).with(not_exists_dir.to_str().unwrap().to_string())).await;
+                let result: Result<_, _> = task.will(Update, once::run(exists_system).with(Args {
+                    path: not_exists_dir,
+                    ..default()
+                })).await;
                 assert!(!result.unwrap());
             }));
         });
@@ -76,7 +92,10 @@ mod tests {
                 let tmp_dir = std::env::temp_dir();
                 let result: Result<_, _> = task.will(Update, {
                     once::res::insert().with(FsScope::default())
-                        .then(once::run(exists_system).with(tmp_dir.to_str().unwrap().to_string()))
+                        .then(once::run(exists_system).with(Args {
+                            path: tmp_dir,
+                            ..default()
+                        }))
                 }).await;
                 result.unwrap_err();
             }));
