28be822c47a59955a1be652dcf6d99b91faf4b7c
not-elm <elmgameinfo@gmail.com>
2025-01-12 15:21:03 +0900

    fix: mouse cursor and keyboard event on Mac
    On Mac, the mouse cursor was not changing and keyboard input wasn't transmitted.

--- crates/core/src/common/plugin/load_webview/protocol.rs
+++ crates/core/src/common/plugin/load_webview/protocol.rs
@@ -50,7 +50,7 @@ fn get_response(
         &path[1..]
     };
     let content = std::fs::read(std::fs::canonicalize(root.join(path))?)?;
-    let mimetype = if path.ends_with(".html") || path == "/" {
+    let mimetype = if path.ends_with(".htm") || path.ends_with(".html") || path == "/" {
         "text/html"
     } else if path.ends_with(".txt") {
         "text/plain"
@@ -88,6 +88,84 @@ fn get_response(
         "video/mp4"
     } else if path.ends_with(".mpeg") {
         "video/mpeg"
+    } else if path.ends_with(".aac") {
+        "audio/aac"
+    } else if path.ends_with(".abw") {
+        "application/x-abiword"
+    } else if path.ends_with(".arc") {
+        "application/x-freearc"
+    } else if path.ends_with(".avi") {
+        "video/m-msvideo"
+    } else if path.ends_with(".azw") {
+        "application/vnd.amazon.ebook"
+    } else if path.ends_with(".bin") {
+        "application/octet-stream"
+    } else if path.ends_with(".bz") {
+        "application/x-bzip"
+    } else if path.ends_with("bz2") {
+        "application/x-bzip2"
+    } else if path.ends_with(".csh") {
+        "application/x-csh"
+    } else if path.ends_with(".doc") {
+        "application/msword"
+    } else if path.ends_with(".docx") {
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    } else if path.ends_with(".eot") {
+        "application/vnd.ms-fontobject"
+    } else if path.ends_with(".epub") {
+        "application/epub+zip"
+    } else if path.ends_with(".gz") {
+        "application/gzip"
+    } else if path.ends_with(".ico") {
+        "image/vnd.microsoft.icon"
+    } else if path.ends_with(".ics") {
+        "text/calendar"
+    } else if path.ends_with(".jar") {
+        "application/java-archive"
+    } else if path.ends_with(".jpeg") || path.ends_with(".jpg") {
+        "image/jpeg"
+    } else if path.ends_with(".mid") || path.ends_with(".midi") {
+        "audio/midi"
+    } else if path.ends_with(".mpkg") {
+        "application/vnd.apple.installer+xml"
+    } else if path.ends_with(".odp") {
+        "application/vnd.oasis.opendocument.presentation"
+    } else if path.ends_with(".ods") {
+        "application/vnd.oasis.opendocument.spreadsheet"
+    } else if path.ends_with(".odt") {
+        "application/vnd.oasis.opendocument.text"
+    } else if path.ends_with(".oga") {
+        "audio/ogg"
+    } else if path.ends_with(".ogv") {
+        "video/ogg"
+    } else if path.ends_with(".ogx") {
+        "application/ogg"
+    } else if path.ends_with(".otf") {
+        "font/otf"
+    } else if path.ends_with(".ppt") {
+        "application/vnd.ms-powerpoint"
+    } else if path.ends_with(".pptx") {
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    } else if path.ends_with(".rar") {
+        "application/vnd.rar"
+    } else if path.ends_with(".rtf") {
+        "application/rtf"
+    } else if path.ends_with(".sh") {
+        "application/x-sh"
+    } else if path.ends_with(".swf") {
+        "application/x-shockwave-flash"
+    } else if path.ends_with(".tif") || path.ends_with(".tiff") {
+        "image/tiff"
+    } else if path.ends_with(".ttf") {
+        "font/ttf"
+    } else if path.ends_with(".vsd") {
+        "application/vnd.visio"
+    } else if path.ends_with(".wav") {
+        "audio/wav"
+    } else if path.ends_with(".weba") {
+        "audio/webm"
+    } else if path.ends_with(".webm") {
+        "video/web"
     } else {
         panic!("not implemented content type {path}");
     };
